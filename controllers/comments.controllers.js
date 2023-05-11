e617e7c2e516739810a2cd6c8dbd8dea717c38eb
cheesenbeans <chrishorsley1@hotmail.co.uk>
2023-05-11 16:32:19 +0100

    this commit fixes only comment from PR to send nothing back not a {}

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,14 +1,12 @@
 const { deleteOneComment } = require("../models/comments.models");
 
 exports.deleteComment = (request, response, next) => {
-    const {comment_id} = request.params
-    deleteOneComment(comment_id)
-    .then(()=>{
-        response.status(204).send({})
+  const { comment_id } = request.params;
+  deleteOneComment(comment_id)
+    .then(() => {
+      response.status(204).send();
     })
-    .catch((err)=>{
-        next(err)
-    })
-
-}
-
+    .catch((err) => {
+      next(err);
+    });
+};
