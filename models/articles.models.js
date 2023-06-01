7df5dd96b934182baa73c2a1f709471bffadb752
cheesenbeans <chrishorsley1@hotmail.co.uk>
2023-06-01 09:19:54 +0100

    order descending comments

--- models/articles.models.js
+++ models/articles.models.js
@@ -111,7 +111,7 @@ exports.postComment = (newComment, articleId) => {
 
 exports.getCommentsByArticle = (articleId) => {
   const articleIdArray = [articleId];
-  let queryStr = `SELECT * FROM comments WHERE article_id=$1 ORDER BY created_at`;
+  let queryStr = `SELECT * FROM comments WHERE article_id=$1 ORDER BY created_at DESC`;
   return connection.query(queryStr, articleIdArray).then((result) => {
     if (result.rows.length === 0) {
       return Promise.reject({ status: 404, msg: "Not Found!" });
